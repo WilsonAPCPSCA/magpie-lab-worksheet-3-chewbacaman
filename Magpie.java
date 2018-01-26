eed4bca2e7a5ab37448a67e876f7c2a1519951dd
Alvin Lee <32201192+chewbacaman@users.noreply.github.com>
2018-01-26 08:51:32 -0800

    Add files via upload

---
+++ Magpie.java
@@ -0,0 +1,146 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie
+{
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+	
+
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		else if (findKeyword(statement, "mother") >= 0
+				|| findKeyword(statement, "father") >= 0
+				|| findKeyword(statement, "sister") >= 0
+				|| findKeyword(statement, "brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		else if (findKeyword(statement, "cat") >= 0 || statement.indexOf("dog") >= 0){
+			response = "Tell me more about your pets";
+		}
+		else if (findKeyword(statement, "Mr.")>=0){
+			response = "Sounds like lit guy";
+		}
+		else if (findKeyword(statement, "Mrs.")>=0||statement.indexOf("Ms. ")>=0){
+			response = "She sounds cool";
+		}
+		else if (statement.length()==0){
+			response = "Please say something";
+		}
+		else if (findKeyword(statement, "dab")>=0){
+			response = "LIT";
+		}
+		else if (findKeyword(statement,"loser")>=0){
+			response = "Ur mom";
+		}
+		else if (findKeyword(statement, "dancing")>=0){
+			response = "DANCING QUEEN. YOUNG AND SWEET, ONLY SEVENTEEN.";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+		
+	}
+	private int findKeyword(String statement, String goal,
+			int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0)
+		{
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword(statement, goal, 0);
+	}
+
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+
+		return response;
+	}
+}
